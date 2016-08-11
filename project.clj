3aa128ab066db9167bff8d250d78c62eff5d84ad
Minh Tran <mtgred@gmail.com>
2016-08-11 20:23:41 +0100

    DB setup

--- project.clj
+++ project.clj
@@ -1,7 +1,9 @@
 (defproject atlas "0.1.0-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/clojurescript "1.8.51"]
+                 [com.novemberain/monger "3.0.2"]
                  [ring "1.5.0"]
+                 [ring/ring-json "0.4.0"]
                  [http-kit "2.1.18"]
                  [bidi "2.0.9"]
                  [reagent "0.5.1"]
--- src/clj/atlas/core.clj
+++ src/clj/atlas/core.clj
@@ -1,22 +1,37 @@
 (ns atlas.core
   (:require [org.httpkit.server :refer [run-server]]
+            [monger.core :as mg]
+            [monger.collection :as mc]
             [ring.util.response :as resp]
+            [ring.middleware.json :as json]
             [bidi.ring :as bidi]))
+
+(defonce server (atom nil))
 
 (defn index-handler [req]
   (resp/file-response "index.html" {:root "resources/public"}))
 
-(def handler
+(defn json-handler [{:keys [route-params]}]
+  (resp/response (map #(dissoc % :_id) (mc/find-maps db (:coll route-params)))))
+
+(def routes
   (bidi/make-handler
-   ["/" [[:get [[""    (bidi/resources-maybe {:prefix "public/"})]
+   ["/" [[["api/" :coll] json-handler]
+         [:get [["" (bidi/resources-maybe {:prefix "public/"})]
                 [#".*" index-handler]]]]]))
 
-(defonce server (atom nil))
+(def handler (-> routes
+                 (json/wrap-json-body {:keywords? true})
+                 json/wrap-json-response))
 
 (defn stop-server []
   (when-not (nil? @server)
     (@server :timeout 100)
+    (mg/disconnect conn)
     (reset! server nil)))
 
 (defn -main [& args]
+  (let [connection (mg/connect-via-uri "mongodb://localhost/atlas")]
+    (def conn (:conn connection))
+    (def db (:db connection)))
   (reset! server (run-server #'handler {:port 1042})))
